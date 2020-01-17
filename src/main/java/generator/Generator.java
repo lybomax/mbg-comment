ccedcd204cec5ca9c4c44b9b241411db1c53aa25
LiYingYing <lybomax@163.com>
2020-01-17 16:14:25 +0800

    1月17

--- src/main/java/generator/Generator.java
+++ src/main/java/generator/Generator.java
@@ -5,7 +5,6 @@ import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,7 +16,7 @@ public class Generator {
     public static void main( String[] args ) throws Exception {
         List<String> warnings = new ArrayList<>();
         ConfigurationParser cp = new ConfigurationParser(warnings);
-        Configuration config = cp.parseConfiguration(Generator.class.getResourceAsStream("/generator/generatorConfig.xml"));
+        Configuration config = cp.parseConfiguration(Generator.class.getResourceAsStream("/generatorConfig.xml"));
         DefaultShellCallback callback = new DefaultShellCallback(true);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
